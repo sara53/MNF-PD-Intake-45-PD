3474ed3cdb49eb2db239e814fdf97aa7c7d90dde
sara <sara.atef001@gmail.com>
2025-01-06 11:31:19 +0200

    day03

---
+++ JS/Day03/demo.js
@@ -0,0 +1,84 @@
+// function add() {
+// 	var test = "Sara";
+// 	console.log(test);
+// 	return "ITI";
+// }
+
+// var result = add();
+// console.log(test);
+// console.log(trackName);
+// console.log(result);
+// // Error 20
+// function sayHello(trackName) {
+// 	console.log(`hello ${trackName} at ITI`);
+// }
+// function greet(trackName) {
+// 	return sayHello(trackName);
+// }
+
+// var result = greet("PD");
+// console.log(result);
+/**==================================== */
+// var name1 = "ali";
+// var name2 = "ahmed";
+// var age1 = 20;
+// var age2 = 25;
+
+// Properties -- Methods
+// property  ==> Key : value (any datatype)
+// access data get set
+// dotNotation
+// squareNotation
+// var person = {}; // new Object() // function constructor
+
+// var person = {
+// 	fname: "ali",
+// 	age: 25,
+// 	address: {
+// 		city: "Alex",
+// 		street: "saad Str",
+// 		code: 124,
+// 	},
+// 	colors: ["red", "green", "blue"],
+// };
+
+// delete person.address;
+// person.gender = "male"; // add
+// person.fname = "ahmed"; //set -- override
+// person["age"] = 20;
+
+// console.log(person);
+
+// console.log(person["address"]["city"]);
+// console.log(person.address.city);
+// console.log(person["address"].city);
+// console.log(person.address["city"]);
+// console.log(person["fname"]);
+// console.log(person["age"]);
+// console.log(person["address"]);
+// get
+// console.log(person.address);
+// console.log(person.gender);
+// console.log(person.fname);
+// console.log(person.age);
+// console.log(person.address);
+// console.log(person.colors);
+// var product = {
+// 	id: 1,
+// 	name: "book",
+// 	price: 200, // method
+// 	discount: function (rate) {
+// 		console.log("Hello");
+// 		console.log(product.price * rate);
+// 	},
+// };
+// console.log(product.id); // prop
+
+// product.discount(0.5); // method
+// console.log(product.discount);
+// var str = "hello";
+
+// console.log(str.length); // prop
+
+// str.charAt() //
+/** DOM --> Document Object Model */
